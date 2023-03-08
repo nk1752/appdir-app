d755784153bc40add9b95308bd06dee8e7f69e67
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-03-08 13:10:58 -0600

    add fetch

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -11,93 +11,65 @@ Amplify.configure(config)
 function Signin() {
 
     const [code, setCode] = useState('')
+    const [jwtToken, setJwtToken] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const count = useRef('')
     const [cognitoUser, setCognitoUser] = useState(null)
 
     async function handleSignIn() {
-    
+            
         try {
-            const signinUser = await Auth.signIn(username, password);
-
-            setCognitoUser(signinUser);
+            const cognitoUser = await Auth.signIn(username, password);
             
-            //console.log(cognitoUser);
-            
-            //localStorage.setItem('signinUser', JSON.stringify(signinUser))
-            //GoToCodePage();
-            
+            console.log(cognitoUser);
+            setCognitoUser(cognitoUser);
+            localStorage.setItem('cognitoUser', JSON.stringify(cognitoUser))
+              //GoToCodePage();
+        
         } catch (error) {
             console.log('error signing in', error);
         }
     
       }
     
-    async function handleConfirmSignIn() {
-
-        //console.log(username)
-        //console.log(code)
-
-        const myUser = localStorage.getItem('signinUser') 
-
-        //console.log(myUser)
+      async function handleConfirmSignIn() {
 
         try {
-          const loggedUser = await Auth.confirmSignIn(myUser, code, 'SMS_MFA')
+          const loggedUser = await Auth.confirmSignIn(cognitoUser, code, 'SMS_MFA')
           
-          //localStorage.setItem('signinUser', JSON.stringify(loggedUser))
+          console.log(code)
+          console.log(loggedUser)
+    
+          const JwtToken = loggedUser.getSignInUserSession()?.getAccessToken()?.getJwtToken() || '';
+    
+          setJwtToken(JwtToken)
+    
+                
         
         } catch (error) {
           console.log('error signing in', error);
         }
       }
+    
 
     return (
         <div className={styles.right}>
             
             
-            <form className={styles.card} onSubmit={handleSignIn}>
-                
-                <h1>Sign In</h1><br />
-
-                <label htmlFor="username">Username:</label><br />
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    required
-                    onChange={(e) => setUsername(e.target.value)}
-                    /><br /><br />
-               
-                <label htmlFor="password">Password:</label><br />
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                    /><br /><br />
-                
-                <button type="submit">Submit</button>
-
-            </form><br />
+            <button type='button' onClick={ handleSignIn }>Sign In</button><br />
+            <button type='button' onClick={ handleConfirmSignIn }>Verify Code</button><br />
            
             
             
             <form  className={styles.card} onSubmit={handleConfirmSignIn}>
                 
-                <label>MFA code: </label><br />
-                <input  type="text" value={code}  onChange={(e) => setCode(e.target.value)} /><br /><br />
+                <label>Username: <input  type="text" value={username}  onChange={(e) => setUsername(e.target.value)} />  </label><br />
+                <label>Password: <input  type="text" value={password}  onChange={(e) => setPassword(e.target.value)} />  </label><br />
 
-                <button type="submit">Verify</button>
+                <label>MFA Code: <input  type="text" value={code}  onChange={(e) => setCode(e.target.value)} />  </label><br />
           
-            </form><br />
-            
-
-            <h1 >username: {username}</h1>
-            <h1 >password: {password}</h1>
-            <p >cognitoUser: {cognitoUser}</p>
+            </form>
 
         </div>
 
