c3425fb448556824aad01f26f2e60bae4fa9c19c
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-05-06 11:57:07 -0500

    update url

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const url = process.env.NEXT_PUBLIC_API_SERVER_URL
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.card}>
 
-        <h1>Hello image tag: github.sha v1</h1>
+        <h1>The URL is: {url}</h1>
       
       
       </div>
