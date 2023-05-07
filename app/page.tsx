29042d346cf5059fac7a1044c7577e04c8e41fb6
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-05-06 19:22:40 -0500

    update NODE_ENV

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 
-const url = process.env.NEXT_PUBLIC_API_SERVER_URL
+const url = 'appdir-service'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.card}>
 
-        <h1>The URL is: {url}</h1>
+        <h1>The process.env.NEXT_PUBLIC_API_SERVER_URL is: {process.env.NODE_ENV}</h1>
       
       
       </div>
